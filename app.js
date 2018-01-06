34fbacfa1c3a9a2595f041efc945862cd69d5669
ziyu <ziyu@caibaopay.com>
2018-01-06 15:22:29 +0800

    ..

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = 'localhost';
-const port = 29334;
+const port = 3000;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
